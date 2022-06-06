5bdb1d5271eda9a4d0807d23936d4108c26f678d
[John Moen] <[gionnym4@gmail.com]>
2022-06-05 22:07:26 -0500

    added bootstrap elements

--- scraping.py
+++ scraping.py
@@ -121,7 +121,10 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes="table table-hover",index=False)
+
+
+
 
 def hemisphere_imgs(browser):
     url = 'https://marshemispheres.com/'
